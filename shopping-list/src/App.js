46e68a6d413b611ed936ab0bac46661b6b3da24e
Addy Hebou <addyhebou@Addys-MacBook-Pro.local>
2021-01-28 22:00:18 -0500

    feat(addItem): can add items

--- shopping-list/src/App.js
+++ shopping-list/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Header from './components/Header'
+import ListItems from './ListItems'
 
 import React, { Component } from 'react'
+// import { library }
 
 export class App extends Component {
   constructor(props) {
@@ -14,21 +16,36 @@ export class App extends Component {
          key:''
        }
     }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
   }
+
 
   // addItem = () =>{
   //   this.setState({items: this.state.items.push("prop")})
   // }
 
-  addItem = () =>{
-    this.setState({items: this.state.items.push(this.state.key)});
+  addItem(e){
+    e.preventDefault(); 
+    const newItem = this.state.currentItem;
+    console.log(newItem.text, newItem.key);
+    if (newItem.text != ""){
+      const items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
   }
 
   handleInput(e){
     this.setState({
       currentItem:{
         text: e.target.value,
-        key: Date.now,
+        key: Date.now(),
       }
     })
   }
@@ -46,6 +63,7 @@ export class App extends Component {
               onChange = {this.handleInput}/>
           <input onClick = {this.addItem} type = "submit"/>
         </form>
+        <ListItems items ={this.state.items} />
       </div>
     )
   }
